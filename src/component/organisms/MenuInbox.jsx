148a1ca250ec236d99554202ed3043bbabdc7e8e
si-saaref <achmadsyarif241@gmail.com>
2023-02-19 19:35:10 +0700

    feat: create search section on menu inbox

--- src/component/organisms/MenuInbox.jsx
+++ src/component/organisms/MenuInbox.jsx
@@ -1,5 +1,7 @@
+import { Input } from 'antd';
 import React, { useState } from 'react';
 import DrawerModal from '../molecules/DrawerModal';
+import { BiSearch } from 'react-icons/bi';
 
 export default function MenuInbox() {
 	const [isOpen, setIsOpen] = useState(false);
@@ -16,8 +18,18 @@ export default function MenuInbox() {
 				Inbox
 			</button>
 			<DrawerModal onClose={() => setIsOpen((prevStae) => !prevStae)} isOpen={isOpen}>
-				<div className='flex justify-center items-center h-full'>
-					<h1>MENU Inbox</h1>
+				<div className='flex justify-center h-full p-2'>
+					<div className='search-wrapper w-full'>
+						<Input
+							placeholder='Search'
+							className='w-full bg-white'
+							suffix={
+								<>
+									<BiSearch />
+								</>
+							}
+						/>
+					</div>
 				</div>
 			</DrawerModal>
 		</>
