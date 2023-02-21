d25fe6d8ad2779bde4106dd480714337d7899fe9
si-saaref <achmadsyarif241@gmail.com>
2023-02-21 15:22:21 +0700

    feat: create message json func to show in inner chat

---
+++ src/utils/messageBot.js
@@ -0,0 +1,12 @@
+export const messageBot = (name) => {
+	return [
+		{
+			sender: 'he',
+			text: "Hello, My Name is Quikerx, with 'X' hehehe, \n What is your name",
+		},
+		{
+			sender: 'he',
+			text: `Whooaaa.. Hi ${name}!! \n How old are you??`,
+		},
+	];
+};
