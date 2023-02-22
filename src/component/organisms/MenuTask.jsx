e00b8f40b6de0fd5bde3c942ce75ad643f49dc9a
si-saaref <achmadsyarif241@gmail.com>
2023-02-22 11:41:33 +0700

    feat: create header comp for menu task

--- src/component/organisms/MenuTask.jsx
+++ src/component/organisms/MenuTask.jsx
@@ -1,3 +1,4 @@
+import { Button, Select } from 'antd';
 import React, { useState } from 'react';
 import DrawerModal from '../molecules/DrawerModal';
 
@@ -16,8 +17,23 @@ export default function MenuTask() {
 				Task
 			</button>
 			<DrawerModal onClose={() => setIsOpen((prevStae) => !prevStae)} isOpen={isOpen}>
-				<div className='flex justify-center items-center h-full'>
-					<h1>MENU TASK</h1>
+				<div className='flex flex-col h-full p-2'>
+					<div className='p-1 flex justify-between'>
+						<Select
+							defaultValue='lucy'
+							style={{ width: 120 }}
+							options={[
+								{ value: 'jack', label: 'Jack' },
+								{ value: 'lucy', label: 'Lucy' },
+								{ value: 'Yiminghe', label: 'yiminghe' },
+								{ value: 'disabled', label: 'Disabled', disabled: true },
+							]}
+						/>
+						<Button type='primary' className='bg-blue-500'>
+							New Task
+						</Button>
+					</div>
+					<div className='flex-grow'>BODY</div>
 				</div>
 			</DrawerModal>
 		</>
